b727835fcec15c56be80030421e50bcef62be42b
Jeremiah Gonzales <119545982+jerbear-calphys@users.noreply.github.com>
2025-02-27 08:56:17 -0800

    Delete routes.py
    fixes to calc.py file make this file unnecessary

--- routes.py
+++
@@ -1,7 +0,0 @@
-from flask import current_app as app
-from . import neucbot
-
-@app.route('/')
-def main_function():
-    neucbot.run_alpha_energy_loss()  # Call the main function from neucbot
-    return f"The result is: {result}"
